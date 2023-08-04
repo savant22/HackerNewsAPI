2844f4c251a8e3dce011264a9fe514308034cf55
savant22 <37748550+savant22@users.noreply.github.com>
2023-08-04 15:12:00 +0530

    Delete HackerNews directory

--- HackerNews/src/test/java/com/example/hack/controllerTest/HackerNewsControllerTest.java
+++
@@ -1,7 +0,0 @@
-package com.example.hack.controllerTest;
-
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest(classes=HackerNewsControllerTest.class)
-public class HackerNewsControllerTest {
-}
